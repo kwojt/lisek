4a9d573c0b749bd04d8e9bf8276cf45b8b5736b5
Krzysztof Wójt <kwojt@g.pl>
2016-11-13 12:21:55 +0100

    Working IMAPClient script for Python3.5

--- lma.py
+++ lma.py
@@ -1,8 +1,27 @@
+#! /usr/bin/env python3
 # Lovehard Management App
-# 0.0.1
-# by kwojt
+# 0.0.4
+# by kwojt(c)
+#########################
 
-from mail.sendmail import send_message
-from mail.createmail import create_message, create_message_with_attachment
+from imapclient import IMAPClient
+# Private settings and credentials
+import mycred
 
+# Getting mail from the server
+mailGet = IMAPClient(mycred.server, use_uid=True, ssl=False)
+mailGet.login(mycred.email, mycred.password)
 
+select_info = mailGet.select_folder("INBOX")
+print('%d messages in INBOX' % select_info[b'EXISTS'])
+
+messages = mailGet.search([b'NOT', b'DELETED'])
+print("%d messages that aren't deleted" % len(messages))
+
+print()
+print("Messages:")
+response = mailGet.fetch(messages, [b'FLAGS', b'RFC822.SIZE'])
+for msgid, data in response.items():
+    print('   ID %d: %d bytes, flags=%s' % (msgid,
+                                            data[b'RFC822.SIZE'],
+                                            data[b'FLAGS']))
